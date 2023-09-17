eed8e3627ce19dd85a33fd8a693281f96ce2c77b
JunKi, Hong <dylanhong920509@gmail.com>
2023-09-17 22:17:31 +0900

    [scripts] add package

--- scripts/dev_env_setup.sh
+++ scripts/dev_env_setup.sh
@@ -8,7 +8,7 @@ sudo apt update
 sudo apt upgrade -y
 sudo apt install    build-essential gcc-multilib g++-multilib gdb cmake llvm lldb clang \
                     libncurses5 libncurses5-dev libncursesw5 libncursesw5-dev bison flex u-boot-tools \
-                    libssl-dev python3-dev python2.7-dev net-tools git vim neovim unzip font-manager \
+                    libssl-dev python3-dev python2.7-dev python3-venv net-tools git vim neovim unzip font-manager \
                     mkisofs neovim curl barrier ssh meson tree \
 					language-pack-en language-pack-ko -y
 
@@ -66,7 +66,7 @@ sudo mkdir -p $NFS_DIR
 sudo chown $USER:$USER $NFS_DIR
 sudo apt install nfs-kernel-server -y
 sudo test -e "/etc/exports" && sudo cp /etc/exports /etc/exports_backup
-sudo echo "$NFS_DIR *(rw,sync,no_root_squash,no_subtree_check)" | sudo tee -a /etc/exports > /dev/null
+sudo echo "$NFS_DIR *(rw,sync,no_root_squash,no_subtree_check,insecure)" | sudo tee -a /etc/exports > /dev/null
 sudo service nfs-kernel-server restart
 
 echo "installing samba..."
